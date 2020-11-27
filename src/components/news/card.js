0906955095b6cc6000fe635dd62a5895c65f53ab
Paweł Bujna <pawelbujna@gmail.com>
2020-11-27 20:00:02 +0100

    change

--- src/components/news/card.js
+++ src/components/news/card.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useTranslation } from "react-i18next"
 
 const { CMS_URL } = process.env
-console.log(process.env)
+
 const Card = ({ item }) => {
   const {
     i18n: { language },
@@ -15,6 +15,7 @@ const Card = ({ item }) => {
           <img src={CMS_URL + item.image[0].url} />
         </figure>
       </div>
+
       <div class="card-content">
         <div class="content">
           <p>{item[`description_${language}`]}</p>
